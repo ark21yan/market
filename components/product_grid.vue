9fa40b75f1835846cadcc77ec078911377efdd9c
ark21yan <120375741+ark21yan@users.noreply.github.com>
2022-12-12 13:23:22 +0000

    abrc 2

--- components/product_grid.vue
+++ components/product_grid.vue
@@ -1,48 +1,21 @@
 <template>
     <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 lg:gap-8 mt-16">
-            <div>
-                <a href="" class="flex flex-col shadow-lg rounded-3xl items-center justify-center bg-white hover:delay-100 transition-all duration-500 hover:scale-105">
-                    <img src="https://res.cloudinary.com/dbvlvmb1r/image/upload/v1667025352/stack-books-vector-doodle-illustration-pile-books-school-library-bookstore-book-stack-sketch-cartoon-isolated-white-background_548264-228_vg5e8h.jpg" class="w-64 h-64 m-4 rounded-xl" alt="">
-                    <p class="my-4">
-                        Книги
-                    </p>
-                </a>
-            </div>
-            <div>
-                <a href="" class="flex flex-col shadow-lg rounded-3xl items-center justify-center bg-white hover:delay-100 transition-all duration-500 hover:scale-105">
-                    <img src="https://res.cloudinary.com/dbvlvmb1r/image/upload/v1667025495/news-103425-image-900x_1621684947_kymlva.jpg" class="w-64 h-64 m-4 rounded-xl" alt="">
-                    <p class="my-4">
-                        Здоровье
-                    </p>
-                </a>
-            </div>
-            <div class="grid grid-rows-2 gap-4">
-                <a href="" class="flex flex-row shadow-lg rounded-3xl items-center justify-center bg-white hover:delay-100 transition-all duration-500 hover:scale-105">
-                    <p class="w-1/2 pl-4">Товары для геймеров</p>
-                    <img src="https://res.cloudinary.com/dbvlvmb1r/image/upload/v1667025525/xtrike-me-gk-402-_D0_98_D0_B3_D1_80_D0_BE_D0_B2_D0_B0_D1_8F-_D0_9A_D0_BB_D0_B0_D0_B2_D0_B8_D0_B0_D1_82_D1_83_D1_80_D0_B0_idh4sr.jpg" class="w-1/2 h-32 m-4 rounded-xl object-contain" alt="">
-                </a>
-                <a href="" class="flex flex-row shadow-lg rounded-3xl items-center justify-center bg-white hover:delay-100 transition-all duration-500 hover:scale-105">
-                    <p class="w-1/2 pl-4">Дача, сезонные товары</p>
-                    <img src="https://res.cloudinary.com/dbvlvmb1r/image/upload/v1667025573/sadovayatechnicakategorydjuly_thsfem.jpg" class="w-1/2 h-32 m-4 rounded-xl object-contain" alt="">
-                </a>
-            </div>
-            <div class="grid grid-rows-2 gap-4">
-                <a href="" class="flex flex-row shadow-lg rounded-3xl items-center justify-center bg-white hover:delay-100 transition-all duration-500 hover:scale-105">
-                    <p class="w-1/2 pl-4">Красота и уход</p>
-                    <img src="https://res.cloudinary.com/dbvlvmb1r/image/upload/v1667025679/apivita-apv-07170-0-300x300_w1lmjy.jpg" class="w-1/2 h-32 m-4 rounded-xl object-contain" alt="">
-                </a>
-                <a href="" class="flex flex-row shadow-lg rounded-3xl items-center justify-center bg-white hover:delay-100 transition-all duration-500 hover:scale-105">
-                    <p class="w-1/2 pl-4">Автотовары</p>
-                    <img src="https://res.cloudinary.com/dbvlvmb1r/image/upload/v1667025709/11_awyotg.jpg" class="w-1/2 h-32 m-4 rounded-xl object-contain" alt="">
-                </a>
-            </div>
-        </div>
+    
+        <div v-for="category in categorys" >
+            <a  href="#" class="flex flex-col shadow-lg rounded-3xl items-center justify-center bg-white hover:delay-100 transition-all duration-500 hover:scale-105">
+                <img src="https://res.cloudinary.com/dbvlvmb1r/image/upload/v1667025525/xtrike-me-gk-402-_D0_98_D0_B3_D1_80_D0_BE_D0_B2_D0_B0_D1_8F-_D0_9A_D0_BB_D0_B0_D0_B2_D0_B8_D0_B0_D1_82_D1_83_D1_80_D0_B0_idh4sr.jpg" alt="">
+                <p class="my-4">
+                    {{category}}
+                </p>
+            </a>
+        </div>            
+    </div>
 </template>
 
 <script setup>
-
+    const { data: categorys} = await useFetch('https://fakestoreapi.com/products/categories?limit=10')
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
